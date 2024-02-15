e46df157d220f8b7b43538a232e91ea9bb30d264
Nisarg Vora <nisarg.vora@me.com>
2024-02-15 17:00:33 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace KeepAwake;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Set up a timer to trigger every minute
+        System.Threading.Timer timer = new System.Threading.Timer(OnTimerCallback, null, TimeSpan.Zero, TimeSpan.FromMinutes(1));
+
+        // Keep the program running
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+
+        // Dispose of the timer when done
+        timer.Dispose();
+    }
+
+    static void OnTimerCallback(object state)
+    {
+        // Simulate pressing F15 key
+        SendKeys.SendWait("{F15}");
+        Console.WriteLine("F15 key pressed at: " + DateTime.Now);
+    }
+}
